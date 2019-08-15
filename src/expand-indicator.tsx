5febce8b01506137868a8b1344a40efde15d27d5
Aaron Edwards <edwards.aaronmark@gmail.com>
2019-08-16 08:16:38 +1000

    Created an expandable indicator

---
+++ src/expand-indicator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ExpandMore } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/styles';
+
+type Props = {
+  expanded: boolean;
+}
+
+const useStyles = makeStyles({
+  root: {
+    transition: "0.25s",
+  },
+  expanded: {
+    transform: "rotate(180deg)",
+  }
+});
+
+const ExpandIndicator = ({expanded}: Props) => {
+  const styles = useStyles();
+  return <ExpandMore className={
+    [styles.root, expanded && styles.expanded]
+      .filter(className => className)
+      .join(' ')
+  }/>
+}
+
+ExpandIndicator.defaultProps = { expanded: false }
+
+export default ExpandIndicator;
---
+++ stories/expand-indicator.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { withKnobs,  boolean} from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+
+import ExpandIndicator from '../src/expand-indicator';
+
+storiesOf('ExapndIndicator', module)
+  .addDecorator(withKnobs)
+  .add('Indicator', () => (
+    <ExpandIndicator expanded={boolean('Expanded', false)}/>
+  ))
